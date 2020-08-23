c62bf0a70a73156fc37126c3507910e65237adaf
programmwriter <programmwriter@mail.ru>
2020-08-23 20:18:59 +0500

    transfer filter

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { sortByPrice, sortByDuration } from "./sorts";
+// import { sortByPrice, sortByDuration, filterByTransfers } from "./sorts";
 
 const initialState = {
   filters: [
@@ -45,8 +46,9 @@ const initialState = {
   sortedTickets: [],
 };
 const toggleTab = (store, id) => {
-  const { tabs, tickets } = store;
-  const tempTickets = id === 1 ? sortByPrice(tickets) : sortByDuration(tickets);
+  const { tabs, sortedTickets } = store;
+  const tempTickets =
+    id === 1 ? sortByPrice(sortedTickets) : sortByDuration(sortedTickets);
 
   const tempTabs = tabs.map((tab) => {
     return tab.id === id ? { ...tab, active: true } : { ...tab, active: false };
@@ -58,46 +60,90 @@ const toggleTab = (store, id) => {
   };
 };
 const switchFilters = ({ filters }, title) => {
+  console.log(title);
+  console.log("title");
+  let resFilters = [];
   // Если включается галочка "Все" - проставляются галочки всем остальным фильтрам
   if (title === "all") {
     const idx = filters.findIndex((el) => el.title === "all");
     const { enabled } = filters[idx];
-    return filters.map((filter) => ({ ...filter, enabled: !enabled }));
+    resFilters = filters.map((filter) => ({ ...filter, enabled: !enabled }));
   }
 
   // активируется необходимая галочка
-  const newFilters = filters.map((filter) => {
+  resFilters = filters.map((filter) => {
     return filter.title === title
       ? { ...filter, enabled: !filter.enabled }
       : filter;
   });
 
   // количество проставленных галочек
-  const countEnabled = newFilters.reduce((acc, filter) => {
+  const countEnabled = resFilters.reduce((acc, filter) => {
     const status = filter.title !== "all" && filter.enabled ? 1 : 0;
     return acc + status;
   }, 0);
 
   // Если проставить каждую галочку по пересадкам - галочка "Все" автоматически включится
-  if (countEnabled === newFilters.length - 1) {
-    return newFilters.map((filter) => {
+  if (countEnabled === resFilters.length - 1) {
+    resFilters = filters.map((filter) => {
       return filter.title === "all" ? { ...filter, enabled: true } : filter;
     });
   }
   // Если при включенной галочке "Все" снимается любая другая галочка - галочка "Все" тоже снимается
-  if (countEnabled !== newFilters.length - 1) {
-    return newFilters.map((filter) => {
+  if (countEnabled !== resFilters.length - 1) {
+    resFilters = filters.map((filter) => {
       return filter.title === "all" ? { ...filter, enabled: false } : filter;
     });
   }
-
-  return newFilters;
+  return resFilters;
 };
+// const switchFilters = ({ filters }, title) => {
+//   // Если включается галочка "Все" - проставляются галочки всем остальным фильтрам
+//   if (title === "all") {
+//     const idx = filters.findIndex((el) => el.title === "all");
+//     const { enabled } = filters[idx];
+//     return filters.map((filter) => ({ ...filter, enabled: !enabled }));
+//   }
+
+//   // активируется необходимая галочка
+//   const newFilters = filters.map((filter) => {
+//     return filter.title === title
+//       ? { ...filter, enabled: !filter.enabled }
+//       : filter;
+//   });
+
+//   // количество проставленных галочек
+//   const countEnabled = newFilters.reduce((acc, filter) => {
+//     const status = filter.title !== "all" && filter.enabled ? 1 : 0;
+//     return acc + status;
+//   }, 0);
+
+//   // Если проставить каждую галочку по пересадкам - галочка "Все" автоматически включится
+//   if (countEnabled === newFilters.length - 1) {
+//     return newFilters.map((filter) => {
+//       return filter.title === "all" ? { ...filter, enabled: true } : filter;
+//     });
+//   }
+//   // Если при включенной галочке "Все" снимается любая другая галочка - галочка "Все" тоже снимается
+//   if (countEnabled !== newFilters.length - 1) {
+//     return newFilters.map((filter) => {
+//       return filter.title === "all" ? { ...filter, enabled: false } : filter;
+//     });
+//   }
+//   return newFilters;
+// };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "CHANGE_FILTER":
-      return { ...state, filters: switchFilters(state, action.title) };
+    case "CHANGE_FILTER": {
+      // const {tickets, tabs} = state;
+      console.log("CHANGE_FILTER");
+      const filters = switchFilters(state, action.title);
+      // console.log(Array.isArray(filters));
+      // const sortedTickets = filterByTransfers(tickets, tabs, filters)
+      return { ...state, filters };
+      // return { ...state, filters, sortedTickets};
+    }
 
     case "TOGGLE_TAB": {
       const { tabs, sortedTickets } = toggleTab(state, action.id);
--- src/reducers/sorts.js
+++ src/reducers/sorts.js
@@ -1,19 +1,110 @@
-export const sortByPrice = (list) => {
-  const tempList = [...list];
+export const sortByPrice = (sortedTickets) => {
+  const tempList = [...sortedTickets];
   return tempList.sort((first, second) =>
     first.price > second.price ? 1 : -1
   );
 };
-export const sortByDuration = (list) => {
-  const tempList = [...list];
+export const sortByDuration = (sortedTickets) => {
+  const tempList = [...sortedTickets];
   return tempList.sort((first, second) =>
     first.segments[0].duration > second.segments[0].duration ? 1 : -1
   );
 };
 
-export const sortByTransfers = (list) => {
-  const tempList = [...list];
-  return tempList.sort((first, second) =>
-    first.price > second.price ? 1 : -1
-  );
+export const filterByTransfers = (tickets, tabs, filters) => {
+  const tempList = [...tickets];
+
+  tempList.filter((ticket) => {
+    const {
+      segments: [first],
+    } = ticket;
+    const { stops } = first;
+    let flag = 0;
+
+    for (const { title, enabled } of filters) {
+      if (enabled) {
+        switch (title) {
+          case "all":
+            flag = 1;
+            break;
+          case "without":
+            flag = stops.length === 0 ? 1 : 0;
+            break;
+          case "one":
+            flag = stops.length === 1 ? 1 : 0;
+            break;
+          case "two":
+            flag = stops.length === 2 ? 1 : 0;
+            break;
+          case "three":
+            flag = stops.length === 3 ? 1 : 0;
+            break;
+          default:
+            flag = 0;
+            break;
+        }
+      }
+    }
+    // for (let index = 0; index < filters.length; index++) {
+    //   const {title, enabled} = filters[index];
+
+    //   if(enabled){
+    //     switch (title) {
+    //       case 'all':
+    //         flag = 1;
+    //         break;
+    //       case 'without':
+    //         flag = stops.length === 0 ? 1 : 0;
+    //         break;
+    //       case 'one':
+    //         flag = stops.length === 1 ? 1 : 0;
+    //         break;
+    //       case 'two':
+    //         flag = stops.length === 2 ? 1 : 0;
+    //         break;
+    //       case 'three':
+    //         flag = stops.length === 3 ? 1 : 0;
+    //         break;
+    //       default:
+    //         flag = 0;
+    //         break;
+    //     }
+    //   }
+
+    // }
+    // filters.forEach(({title, enabled}) => {
+
+    //   if(enabled){
+    //     switch (title) {
+    //       case 'all':
+    //         flag = 1;
+    //         break;
+    //       case 'without':
+    //         flag = stops.length === 0 ? 1 : 0;
+    //         break;
+    //       case 'one':
+    //         flag = stops.length === 1 ? 1 : 0;
+    //         break;
+    //       case 'two':
+    //         flag = stops.length === 2 ? 1 : 0;
+    //         break;
+    //       case 'three':
+    //         flag = stops.length === 3 ? 1 : 0;
+    //         break;
+    //       default:
+    //         flag = 0;
+    //         break;
+    //     }
+    //   }
+    // });
+    return flag;
+  });
+
+  // сортируем по количеству остановок
+  const filteredTickets = filterByTransfers(tickets);
+
+  const sortedTickets = tabs[0].active
+    ? sortByPrice(filteredTickets)
+    : sortByDuration(filteredTickets);
+  return sortedTickets;
 };
