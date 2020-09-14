e0d70c366ad46d5ab4a1552109291e586c1ec242
programmwriter <programmwriter@mail.ru>
2020-09-14 15:31:43 +0500

    redux devtools enabled in prod

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
-import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import rootReducer from "./reducers";
 
